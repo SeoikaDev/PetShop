f0e9eb7b708c4afc6cb4c91ceb87dfc226c123e9
Khang <h.v.khang68@gmail.com>
2022-04-07 08:01:10 +0700

    implement product functions

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,23 +1,63 @@
+const { json } = require('express/lib/response');
 const ProductModel = require('../models/Product')
 
 //Get all products
 const getProducts = async(req, res, next) => {
     try {
         const products = await ProductModel.find();
-        return res.json(products)
+        return res.json({ "status": "ok", "data": products })
     } catch (error) {
-        res.status(404).json(error.message);
+        return res.json({ "status": "error", "error": error.message })
     }
 }
 
 //Get product by id
 const getProductById = async(req, res, next) => {
     try {
-        const product = await ProductModel.findById()
+        const _id = req.params.productId
+        const product = await ProductModel.findById(_id)
+        return res.json({ "status": "ok", "data": product })
     } catch (error) {
-        return res.status(404).json(error.message);
+        return res.json({ "status": "error", "error": error.message })
+    }
+}
+
+//Add product
+const addProduct = async(req, res, next) => {
+    try {
+        const product = req.body
+        await ProductModel.create(product);
+        return res.json({ "status": "ok", "info": "Add product successfully" })
+    } catch (error) {
+        return res.json({ "status": "error", "error": error.message })
+    }
+}
+
+//Update product
+const updateProduct = async(req, res, next) => {
+    try {
+        const product = req.body;
+        await ProductModel.updateOne({ _id: product._id }, product);
+        return res.json({ "status": "ok", "info": "Update product successfully" })
+    } catch (error) {
+        return res.json({ "status": "error", "error": error.message })
+    }
+}
+
+//Delete product
+const deleteProduct = async(req, res, next) => {
+    try {
+        const product = req.body;
+        await ProductModel.deleteOne({ _id: product._id });
+        return res.json({ "status": "ok", "info": "Delete product successfully" })
+    } catch (error) {
+        return res.json({ "status": "error", "error": error.message })
     }
 }
 module.exports = {
-    getProducts
+    getProducts,
+    getProductById,
+    addProduct,
+    updateProduct,
+    deleteProduct
 }

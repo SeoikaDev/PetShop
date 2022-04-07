de89b1fcc862e279feb5f67fa3dca8efb91eb5ad
Khang <h.v.khang68@gmail.com>
2022-04-07 11:46:00 +0700

    add change user information function

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,14 +1,36 @@
+const { json } = require('express/lib/response');
 const UserModel = require('../models/User');
+const jwt = require('jsonwebtoken')
 
 const getUsers = async(req, res, next) => {
     try {
         const users = await UserModel.find().populate('cart.product');
-        res.send(users);
+        return res.json({ "status": "ok", "data": users })
     } catch (error) {
-        res.send(error.message);
+        return res.json({ "status": "error", "error": error.message })
     }
 }
 
+const changeUserInformation = async(req, res, next) => {
+    try {
+        const { full_name, dob, phone_number } = req.body;
+        const email = jwt.decode(req.headers['x-access-token']).email;
+        const user = await UserModel.findOne({ email: email }).lean()
+        if (!user) {
+            return res.json({ "status": "error", "error": "Could not found this user" });
+        }
+        user.full_name = full_name;
+        user.dob = dob;
+        user.phone_number = phone_number;
+        await UserModel.updateOne({ email: email }, user);
+        return res.json({ "status": "ok", "info": "User information changed" })
+    } catch (error) {
+        return res.json({ "status": "error", "error": error.message })
+    }
+
+}
+
 module.exports = {
-    getUsers
+    getUsers,
+    changeUserInformation
 }

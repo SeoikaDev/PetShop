75fb6381dba68161d774e351ada23943954c5683
Khang <h.v.khang68@gmail.com>
2022-04-07 08:02:08 +0700

    add product and user model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, select: true }
+    password: { type: String, required: true, select: true },
+    verification_code: { type: String, required: false },
+    role: {
+        type: String,
+        required: true
+    }
 }, { collection: 'users' });
 
 module.exports = mongoose.model('User', UserSchema);

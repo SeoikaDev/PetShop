05edb793e6bcaab6278518ce6db2b6cf2ef21ae9
Khang <h.v.khang68@gmail.com>
2022-04-07 11:46:31 +0700

    add change user information to user routes

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router()
 const UserController = require('../controllers/UserController')
+const auth_middleware = require('../middleware/auth')
 
 //Get all route
 
-router.get('/users', UserController.getUsers)
+router.get('/users', auth_middleware.verify_admin_role, UserController.getUsers)
+
+router.post('/users/change-user-information', auth_middleware.verify_user_role, UserController.changeUserInformation)
 
 module.exports = {
     "router": router
